c6192207c1d4fa00141bcc6031af0cfa7467864b
UxuginPython <84234447+UxuginPython@users.noreply.github.com>
2022-04-19 19:25:58 -0600

    Uploaded files

---
+++ MAIN.py
@@ -0,0 +1,32 @@
+print('Uxugin Analyze 1.0.0')
+
+import numpy
+import scipy
+import scipy.stats
+
+fileName=input('File: ')
+file=open(fileName, 'r')
+data=file.read().split('\n')
+file.close()
+for i in range(len(data)):
+    data[i]=float(data[i])
+#print(data)
+    
+mean=numpy.mean(data)
+print('Mean: '+str(mean))
+
+median=numpy.median(data)
+print('Median: '+str(median))
+
+mode=scipy.stats.mode(data)
+mode=mode.mode[0]
+print('Mode: '+str(mode))
+
+deviations=[]
+for i in data:
+    deviations.append(numpy.abs(i-mean))
+mad=numpy.mean(deviations)
+print('MAD: '+str(mad))
+
+iqr=scipy.stats.iqr(data)
+print('IQR: '+str(iqr))
