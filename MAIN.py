912510a342cd7ff58b9f49d090e7d07e038c8dc9
UxuginPython <84234447+UxuginPython@users.noreply.github.com>
2022-05-05 07:11:00 -0600

    Added GUI

--- MAIN.py
+++ MAIN.py
@@ -1,35 +1,47 @@
-print('Uxugin Analyze 1.1.0')
+VERSION='1.2.0'
+from tkinter import *
+from tkinter import ttk
+from tkinter.filedialog import askopenfilename
 
 import numpy
 import scipy
 import scipy.stats
 
-fileName=input('File: ')
+fileName=askopenfilename()
 file=open(fileName, 'r')
 data=file.read().split('\n')
 file.close()
 for i in range(len(data)):
     data[i]=float(data[i])
 #print(data)
+
+window=Tk()
+window.title('Uxugin Analyze '+VERSION)
+window.iconbitmap('Analyze.ico')
+
+def showThing(text):
+    Label(window, text=text).pack()
     
 mean=numpy.mean(data)
-print('Mean: '+str(mean))
+showThing('Mean: '+str(mean))
 
 median=numpy.median(data)
-print('Median: '+str(median))
+showThing('Median: '+str(median))
 
 mode=scipy.stats.mode(data)
 mode=mode.mode[0]
-print('Mode: '+str(mode))
+showThing('Mode: '+str(mode))
 
 deviations=[]
 for i in data:
     deviations.append(numpy.abs(i-mean))
 mad=numpy.mean(deviations)
-print('MAD: '+str(mad))
+showThing('MAD: '+str(mad))
 
 iqr=scipy.stats.iqr(data)
-print('IQR: '+str(iqr))
+showThing('IQR: '+str(iqr))
 
 std=numpy.std(data)
-print('Standard Deviation: '+str(std))
+showThing('Standard Deviation: '+str(std))
+
+mainloop()
